43e91a4397935665d9f56a00f810de42fee1ea3a
BambooAce <1774390393@qq.com>
2017-08-21 17:53:04 +0800

    update task.c
    modify some bugs

--- pthreadPool/task.c
+++ pthreadPool/task.c
@@ -6,6 +6,7 @@ int initTaskList(TASKLIST *tl)
 {
     tl->header = NULL;
     tl->num = 0;
+    pthread_mutex_init(tl->mutex, NULL);
     return 1;
 }
 
@@ -14,10 +15,14 @@ TASKPACKAGE *getATask(TASKLIST *tl)
     assert(tl);
     if(tl->num)
     {
+        TaskNode *tn = tl->header;
+        TaskNode *tnn = tn;
         int i;
         for(i = 0; i < tl->num; ++i)
         {
-            tl->header->
+            tnn = tn->next;
+            tnn->priority ;
+            tn = tnn;
         }
     }
     return NULL;
@@ -32,20 +37,23 @@ int putATask(TASKLIST *tl, ExecuteTask runtask, void *arg, unsigned int argsize,
     }else
         tp->arg = NULL;
     tp->et = runtask;
+    tp->status = 0;
     TaskNode *atask = (TaskNode *)malloc(sizeof(TaskNode));
     if(atask){
         atask->tg = tp;
         atask->priority = priority > 0 ? priority : 1;
+        pthread_mutex_lock(tl->mutex);
         if(tl->header){
             tl->header->pretask = atask;
             atask->next = tl->header;
-            atask->pretask = tl;
+            atask->pretask = NULL;
         }else{
             tl->header = atask;
             atask->pretask = NULL;
             atask->next = NULL;
         }
         tl->num++;
+        pthread_mutex_unlock(tl->mutex);
         return 1;
     }
     return 0;
@@ -54,11 +62,15 @@ int putATask(TASKLIST *tl, ExecuteTask runtask, void *arg, unsigned int argsize,
 
 int clearTaskList(TASKLIST *tl)
 {
+    assert(tl);
+    pthread_mutex_lock(tl->mutex);
     if(tl->num)
     {
         TaskNode * tn = tl->header;
-        while(tn)
+        TaskNode * tnn = tn;
+        while(tn && tl->num)
         {
+            tnn = tn->next;
             if(tn->tg->arg){
                 free(tn->tg->arg);
                 tn->tg->arg = NULL;
@@ -69,25 +81,53 @@ int clearTaskList(TASKLIST *tl)
                 tn->tg = NULL;
             }
             free(tn);
-            tn = NULL;
-            tn = tn->next;
+            tn = tnn;
             tl->num--;
         }
     }
+    tl->header = NULL;
+    tl->num = 0;
+    pthread_mutex_unlock(tl->mutex);
     return 1;
 }
 
-int deleteTask(TASKLIST *tl, TASKPACKAGE tg)
+int deleteTask(TASKLIST *tl, ExecuteTask _tg)
 {
     assert(tl);
     if(tl->num)
     {
         TaskNode *tn = tl->header;
-        while(tn)
+        TaskNode *tnn = tn;
+        while(tn && tl->num)
         {
+            tnn = tn->next;
             if(tn->tg){
-                memcmp()
+                if(tn->tg->et == _tg)
+                {
+                    if(!tn->pretask)
+                    {
+                        tl->header = tn->next;
+                    }else{
+                        tn->pretask->next = tn->next;
+                        tn->next->pretask = tn->pretask;
+                    }
+                    if(tn->tg->arg){
+                        free(tn->tg->arg);
+                        tn->tg->arg = NULL;
+                    }
+                    free(tn->tg);
+                    tn->tg = NULL;
+                    free(tn);
+                    tn = NULL;
+                    tl->num--;
+                    if(tl->num == 0)
+                    {
+                        tl->header = NULL;
+                        break;
+                    }
+                }
             }
+            tn = tnn;
         }
     }
     return 1;
