e0e0a0af5afda41648bb97eb1758e161e723856a
BambooAce <1774390393@qq.com>
2017-08-21 17:51:44 +0800

    update task.h
    aad pthread lock

--- pthreadPool/task.h
+++ pthreadPool/task.h
@@ -1,3 +1,4 @@
+#include <pthread.h>
 typedef void (*ExecuteTask)(void **arg);
 
 typedef struct taskpackage{
@@ -6,21 +7,22 @@ typedef struct taskpackage{
     int status;
 }TASKPACKAGE;
 
-typedef struct{
+typedef struct TASKNODE{
     int priority;
     TASKPACKAGE *tg;
-    TASKPACKAGE *pretask;
-    TASKPACKAGE *next;
+    TASKNODE *pretask;
+    TASKNODE *next;
 }TaskNode;
 
 typedef struct {
     TaskNode * header;
     unsigned int num;
+    pthread_mutex_t * mutex;
 }TASKLIST;
 
 int initTaskList(TASKLIST *tl);
 TASKPACKAGE * getATask(TASKLIST *tl);
 int putATask(TASKLIST *tl, ExecuteTask runtask, void *arg, unsigned int argsize, int priority);
-int deleteTask(TASKLIST *tl, TASKPACKAGE tg);
+int deleteTask(TASKLIST *tl, ExecuteTask _tg);
 int clearTaskList(TASKLIST *tl);
 int getTaskNum(TASKLIST * tl);
